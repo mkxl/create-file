fda2dbbb125077b5a7a4b7a802c969c336de0f19
michael <66692526+mkxl@users.noreply.github.com>
2026-08-06 01:44:08 -0400

    add flake.{nix,lock} files

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    crane.url = "github:ipetkov/crane";
+    fenix = {
+      url = "github:nix-community/fenix/monthly";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs =
+    {
+      self,
+      crane,
+      fenix,
+      flake-utils,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      (
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          rust-toolchain = fenix.packages.${system}.fromToolchainFile {
+            file = ./rust-toolchain.toml;
+            sha256 = "sha256-K8/aNzEwNFy5A+HIFCFhHbilHJezC3HqOc9YItLeZ7c=";
+          };
+          crane-lib = (crane.mkLib pkgs).overrideToolchain (pkgs: rust-toolchain);
+        in
+        {
+          packages.default = crane-lib.buildPackage {
+            src = crane-lib.cleanCargoSource ./.;
+          };
+          devShells.default = crane-lib.devShell {};
+          formatter = pkgs.nixfmt;
+        }
+      )
+    );
+}
